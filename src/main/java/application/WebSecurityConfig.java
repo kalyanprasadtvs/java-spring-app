1652ae4b8b862de2c71efbfabca35268a2f20861
kalyanprasadtvs <kalyanprasadtvs@gmail.com>
2022-09-22 20:29:41 +0530

    Update WebSecurityConfig.java

--- src/main/java/application/WebSecurityConfig.java
+++ src/main/java/application/WebSecurityConfig.java
@@ -9,22 +9,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http
-        .anonymous()
-            .authorities("ROLE_ANONYMOUS")
-            .and()
-        .headers()
-             .cacheControl()
-             .and()
-        .authorizeRequests()
-            .antMatchers("/").permitAll()
-            .antMatchers("/profile/image").permitAll()
-            .antMatchers("/favicon.ico").permitAll()
-            .antMatchers("/resources/**").permitAll()
-
-            .antMatchers(HttpMethod.GET, "/login/**").permitAll()
-
-            .anyRequest().authenticated();
+        http.csrf().disable()
+                .anonymous().authorities("ROLE_ANONYMOUS")
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/login/**").permitAll();
     }
 }
