de811f51b9fd59fb042398a3bc0ce4e33e75bb48
kalyanprasadtvs <kalyanprasadtvs@gmail.com>
2022-09-26 22:36:38 +0530

    Update WebSecurityConfig.java

--- src/main/java/application/WebSecurityConfig.java
+++ src/main/java/application/WebSecurityConfig.java
@@ -1,36 +1,36 @@
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.http.HttpMethod;
+// import org.springframework.http.HttpMethod;
 
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+// @EnableWebSecurity
+public class WebSecurityConfig { //extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception {
 //         http.csrf().disable()
 //                 .anonymous().authorities("ROLE_ANONYMOUS")
 //                 .and()
 //                 .authorizeRequests()
 //                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 //                 .antMatchers(HttpMethod.GET,"/login/**").permitAll();
-        http.csrf().disable()
-            .anonymous()
-            .authorities("ROLE_ANONYMOUS")
-            .and()
-            .authorizeRequests()            
-            .antMatchers("/").permitAll()
-            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/login/**").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin().loginPage("/login").permitAll()
-            .and()
-            .headers()
-            .cacheControl();
-        http.formLogin().disable();
-    }
+//         http.csrf().disable()
+//             .anonymous()
+//             .authorities("ROLE_ANONYMOUS")
+//             .and()
+//             .authorizeRequests()            
+//             .antMatchers("/").permitAll()
+//             .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//             .antMatchers(HttpMethod.GET, "/login/**").permitAll()
+//             .anyRequest().authenticated()
+//             .and()
+//             .formLogin().loginPage("/login").permitAll()
+//             .and()
+//             .headers()
+//             .cacheControl();
+//         http.formLogin().disable();
+//     }
 }
