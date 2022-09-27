7239310501b472af1abf7f0e402a222661c2e0cc
kalyanprasadtvs <kalyanprasadtvs@gmail.com>
2022-09-27 12:40:50 +0530

    Update WebSecurityConfig.java

--- src/main/java/application/WebSecurityConfig.java
+++ src/main/java/application/WebSecurityConfig.java
@@ -6,8 +6,30 @@
 
 // import org.springframework.http.HttpMethod;
 
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 // @EnableWebSecurity
-public class WebSecurityConfig { //extends WebSecurityConfigurerAdapter {
+@Component
+public class WebSecurityConfig implements Filter { //extends WebSecurityConfigurerAdapter {
+  
+    @Override
+    public void destroy() {
+        System.out.println("destroy filter. release our resources here if any");
+    }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
+                                                                                              IOException,ServletException {
+        HttpServletResponse httpServletResponse=(HttpServletResponse)response;
+        httpServletResponse.setHeader("Custom-Filter-Header","Write Header using Filter");
+        chain.doFilter(request, response);      // continue execution of other filter chain.
+    }
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Init filter");
+    }
 
 //     @Override
 //     protected void configure(HttpSecurity http) throws Exception {
